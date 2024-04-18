2611cc64c5fcdfea69095a9ce8157c6811af5e12
Hadi Hedjam <hhedjam@bess-f4r6s5.clusters.42paris.fr>
2024-04-18 19:06:23 +0200

    launch

---
+++ minitalk/utils.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhedjam <hhedjam@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 18:53:09 by hhedjam           #+#    #+#             */
+/*   Updated: 2024/04/18 18:59:58 by hhedjam          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <signal.h>
+
+void	print(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	sig_handler(int signum)
+{
+	if (signum == SIGINT)
+	{
+		print("Signal SIGINT received\n");
+	}
+	else if (signum == SIGQUIT)
+	{
+		print("Signal SIGQUIT received\n");
+	}
+}
+
+/*int	main()
+{
+	sig_handler(SIGINT);
+	return (0);
+}
+*/
+
