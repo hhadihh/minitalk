9e4654809b1518f76fcd6ff38b80e461b07df701
Hadi Hedjam <hhedjam@bess-f1r1s6.clusters.42paris.fr>
2024-05-02 17:43:25 +0200

    justthat

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,51 @@
 /*   By: hhedjam <hhedjam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 17:15:09 by hhedjam           #+#    #+#             */
-/*   Updated: 2024/05/02 17:15:11 by hhedjam          ###   ########.fr       */
+/*   Updated: 2024/05/02 17:42:25 by hhedjam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int	main (int argc, char **argv)
+{
+	int		pid;
+	int		i;
+	char	*str;
+
+	if (argc != 3)
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
+	pid = ft_atoi(argv[1]);
+	str = argv[2];
+	i = 0;
+	while (str[i])
+	{
+		send_char(pid, str[i]);
+		i++;
+	}
+	send_char(pid, '\0');
+	return (0);
+}
+
+void	send_char(int pid, char c)
+{
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (c & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		c >>= 1;
+		usleep(100);
+		i++;
+	}
+}
