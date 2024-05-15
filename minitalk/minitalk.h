1dca75ea8397eabf3927ad2270b4154693e77ea5
Hadi Hedjam <hhedjam@paul-f4Ar5s9.clusters.42paris.fr>
2024-05-15 14:27:52 +0200

    norme2presque

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -6,18 +6,18 @@
 /*   By: hhedjam <hhedjam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 17:44:04 by hhedjam           #+#    #+#             */
-/*   Updated: 2024/05/10 17:47:50 by hhedjam          ###   ########.fr       */
+/*   Updated: 2024/05/15 14:25:11 by hhedjam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
-#define MINITALK_H
+# define MINITALK_H
 
-#include <signal.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
 
 // Function declarations
 void	send_char(int pid, char c);
